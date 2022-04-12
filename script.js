1181384a8951f1d5e51fcc52ae6f4be18a003696
Princo1-bit <64435245+Princo1-bit@users.noreply.github.com>
2022-04-12 12:50:16 +0100

    hopeful update

--- script.js
+++ script.js
@@ -13,16 +13,16 @@ document.querySelector(".right").addEventListener("click", ()=> {
 
 
     if(counter == 1){
-        document.querySelector('.header').style.backgroundImage = "url(./img/pexels-quintin-gellar-2199293.jpg)";
+        document.querySelector('.header').style.backgroundImage = "url(../img/pexels-quintin-gellar-2199293.jpg)";
         document.querySelector(".info h1").innerHTML = "International delivery";
         document.querySelector(".info h3").innerHTML = "4 days - 150 countries";
 
     } else if(counter == 2){
-        document.querySelector('.header').style.backgroundImage = "url(./img/pexels-martin-damboldt-799091.jpg)";
+        document.querySelector('.header').style.backgroundImage = "url(../img/pexels-martin-damboldt-799091.jpg)";
         document.querySelector(".info h1").innerHTML = "We are proud to";
         document.querySelector(".info h3").innerHTML = "be always on demand";
     } else if(counter ==3){
-        document.querySelector('.header').style.backgroundImage = "url(./img/pexels-pixabay-163771.jpg)";
+        document.querySelector('.header').style.backgroundImage = "url(../img/pexels-pixabay-163771.jpg)";
         document.querySelector(".info h1").innerHTML = "Providing the highest quality";
         document.querySelector(".info h3").innerHTML = "service 24 hours a day";
     } else if(counter > 3) {
@@ -40,16 +40,16 @@ document.querySelector(".left").addEventListener("click", ()=> {
     
     
     if(counter == 1){
-        document.querySelector('.header').style.backgroundImage = "url(./img/pexels-quintin-gellar-2199293.jpg)";
+        document.querySelector('.header').style.backgroundImage = "url(../img/pexels-quintin-gellar-2199293.jpg)";
         document.querySelector(".info h1").innerHTML = "International delivery";
         document.querySelector(".info h3").innerHTML = "4 days - 150 countries";
 
     } else if(counter == 2){
-        document.querySelector('.header').style.backgroundImage = "url(./img/pexels-martin-damboldt-799091.jpg)";
+        document.querySelector('.header').style.backgroundImage = "url(../img/pexels-martin-damboldt-799091.jpg)";
         document.querySelector(".info h1").innerHTML = "We are proud to";
         document.querySelector(".info h3").innerHTML = "be always on demand";
     } else if(counter ==3){
-        document.querySelector('.header').style.backgroundImage = "url(./img/pexels-pixabay-163771.jpg)";
+        document.querySelector('.header').style.backgroundImage = "url(../img/pexels-pixabay-163771.jpg)";
         document.querySelector(".info h1").innerHTML = "Providing the highest quality";
         document.querySelector(".info h3").innerHTML = "service 24 hours a day";
     } else if(counter < 1) {
